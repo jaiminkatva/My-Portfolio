9993eec6dfde2323834039505bce7cb9b697c6e3
Build India <jaiminkatva@gmail.com>
2026-09-23 12:52:55 +0530

    change vite config according github page deployment

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  base: "/",
+
   server: {
     port: 5173,
   },
+
   build: {
-    target: 'es2020',
+    target: "es2020",
     sourcemap: false,
   },
 });
